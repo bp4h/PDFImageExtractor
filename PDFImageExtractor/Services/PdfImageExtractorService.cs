e40d1b57e60fab848d1d539b4e6fa1cc37cd0de6
bp4h <al.logvinov@gmail.com>
2023-11-08 17:20:53 +0100

    Add project files.

---
+++ PDFImageExtractor/Services/PdfImageExtractorService.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using Spire.Pdf;
+
+
+namespace PDFImageExtractor.Services
+{
+
+    public class PdfImageExtractorService
+    {
+        static public List<Image> ExtractImagesFromPdf(Stream pdfStream)
+        {
+            List<Image> images = new List<Image>();
+
+            PdfDocument pdf = new PdfDocument();
+
+            pdf.LoadFromStream(pdfStream);
+
+            int i = 1;
+
+            foreach (PdfPageBase page in pdf.Pages)
+            {
+                foreach (Image image in page.ExtractImages())
+                {
+                    images.Add(image);
+                    i++;
+                }
+            }
+
+            return images;
+        }
+    }
+}
+
